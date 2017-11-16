2c82a8cdcdbf2aa79630c5ef481f3998115762ee
Dilong-D <dmarkowski19@gmail.com>
2017-11-17 00:11:20 +0100

    ready multimaster interface

---
+++ src/main/java/Config.java
@@ -0,0 +1,21 @@
+public class Config {
+
+    public final static int OWN_SLAVE_ID=202;
+    public static final int PORT = 5555;
+
+    public static final String CONTROLLER_IP = "192.168.1.111";//"192.168.1.69";
+    public static final int CONTROLLER_SLAVE_ID=111;
+
+    public static final String LOGGER_IP = "192.168.1.111";//"192.168.1.222";
+    public static final int LOGGER_SLAVE_ID=222;
+
+    public static final int T_ZCO_REGISTER= 200;
+    public static final int T_COB_REGISTER=420;
+    public static final int F_COB_REGISTER=422;
+    public static final int UB_REGISTER = 424;
+    public static final int T_R_REGISTER = 426;
+    public static final int T_O_REGISTER = 100;
+    public static final int TIMER_0_REGISTER = 0;
+    public static final int TIMER_1_REGISTER = 1;
+    public static final int TIMER_FLAG_REGISTER = 0;
+}
---
+++ src/main/java/ModbusInterface.java
@@ -0,0 +1,102 @@
+import com.serotonin.modbus4j.BasicProcessImage;
+import com.serotonin.modbus4j.exception.IllegalDataAddressException;
+import com.serotonin.modbus4j.exception.ModbusInitException;
+import com.serotonin.modbus4j.ip.tcp.TcpSlave;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ModbusInterface {
+
+    private static BasicProcessImage basicProcImage;
+
+
+    /*
+        1. Zegar (Mieszko) wpisuje do mojego rejestru (#0) czas.
+        2. Zegar (Mieszko) wpisuje do mojego rejestru flagi gotowości (#0).
+        3. Zegar (Mieszko) czyta z rejestru flage gotowosci
+        4. Wymiennik(Michaly) wpisuje do budynków temperature wody w systemie(#200 - Tzco - woda wchodzaca do budynku).
+        5. Budynek(Ja) wpisuje regulatorowi(Mariusz) rejestry (#420 - Tcob - woda wychodzaca z budynku i #422 - Fcob - strumien wody wychodzacej).
+        6. Budynek(Ja) wpisuje Loggerowi (Jędrzej) (#420 - Tcob - woda wychodzaca z budynku, #422 - Fcob - strumien wody wychodzacej i #424 - Ub2 - stopien otwarcia zaworów i #426- Tr-temp budynku)).
+     */
+    private static int time_0; //#0
+    private static int time_1;//#1
+    private static boolean time_flag;//#0
+    private static int To;//#100
+    private static int Tzco; //#200
+    private static int Tcob; //#420
+    private static int Fcob; //#422
+    private static int Ub2; //#424
+    private static int Tr;//#426
+
+    public static String toStringStatic() {
+        return "time_0: " + time_0 + " time_1: " + time_1 + " time_flag: " + time_flag + " Tzco: " + Tzco + " Tcob: " + Tcob + " Fcob: " + Fcob + " Ub2: " + Ub2 + " Tr: " + Tr + " To: " + To;
+    }
+
+    public static void main(String[] args) {
+
+        TcpSlave slave = initSlave();
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                    try {
+                        slave.start();
+                    } catch (ModbusInitException ex) {
+                        Logger.getLogger(ModbusInterface.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            public void run() {
+                Master master = new Master();
+                while (true) {
+                    try {
+                        time_flag = basicProcImage.getCoil(0);
+                        if (time_flag == true) {
+                            getRegistersValue();
+                            master.sendDataToController(Tcob, Fcob);
+                            master.sendDataToLogger(Tcob, Fcob, Ub2, Tr);
+                        }
+
+                    } catch (IllegalDataAddressException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(toStringStatic());
+                }
+            }
+        }).start();
+
+    }
+
+    private static void getRegistersValue() throws IllegalDataAddressException {
+        time_flag = basicProcImage.getCoil(Config.TIMER_FLAG_REGISTER);
+        time_0 = basicProcImage.getHoldingRegister(Config.TIMER_0_REGISTER);
+        time_1 = basicProcImage.getHoldingRegister(Config.TIMER_1_REGISTER);
+        Tzco = basicProcImage.getHoldingRegister(Config.T_ZCO_REGISTER);
+        Tcob = basicProcImage.getHoldingRegister(Config.T_COB_REGISTER);
+        Fcob = basicProcImage.getHoldingRegister(Config.F_COB_REGISTER);
+        Ub2 = basicProcImage.getHoldingRegister(Config.UB_REGISTER);
+        Tr = basicProcImage.getHoldingRegister(Config.T_R_REGISTER);
+        To = basicProcImage.getHoldingRegister(Config.T_O_REGISTER);
+
+    }
+
+    private static TcpSlave initSlave(){
+        basicProcImage = new BasicProcessImage(Config.OWN_SLAVE_ID);
+        basicProcImage.setCoil(Config.TIMER_FLAG_REGISTER,false);
+        basicProcImage.setHoldingRegister(Config.TIMER_0_REGISTER, (short) 0);
+        basicProcImage.setHoldingRegister(Config.TIMER_1_REGISTER, (short) 0);
+        basicProcImage.setHoldingRegister(Config.T_ZCO_REGISTER, (short) 0);
+        basicProcImage.setHoldingRegister(Config.T_COB_REGISTER, (short) 0);
+        basicProcImage.setHoldingRegister(Config.F_COB_REGISTER, (short) 0);
+        basicProcImage.setHoldingRegister(Config.UB_REGISTER, (short) 0);
+        basicProcImage.setHoldingRegister(Config.T_R_REGISTER, (short) 0);
+        basicProcImage.setHoldingRegister(Config.T_O_REGISTER, (short) 0);
+        TcpSlave slave = new TcpSlave(Config.PORT, false);
+        slave.addProcessImage(basicProcImage);
+        return slave;
+    }
+}
